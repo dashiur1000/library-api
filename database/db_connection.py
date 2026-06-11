890f754199f7c41d0aec0298432afaf8538342bf
david stern <dzs101010@gmail.com>
2026-06-11 21:04:50 +0300

    create db_connection

--- database/db_connection.py
+++ database/db_connection.py
@@ -2,9 +2,10 @@ import mysql.connector
 
 conn = mysql.connector.connect(
     host="localhost",
-    user="yourusername",
-    password="yourpassword",
-    database="mydatabase"
+    port=3309,
+    user="root",
+    password="secret",
+    database="library_db"
 )
 
 cursor = conn.cursor()
@@ -14,7 +15,25 @@ CREATE TABLE IF NOT EXISTS books (
 id INT AUTO_INCREMENT PRIMARY KEY,
 title VARCHAR(50) UNIQUE,
 author VARCHAR(50) UNIQUE,
-genre VARCHAR(255) ["Fiction", "Non-Fiction", "Science", "History", "Other"],
-is_available BOOLEAN,
+genre ENUM('Fiction', 'Non-Fiction', 'Science', 'History', 'Other'),
+is_available BOOLEAN DEFAULT TRUE,
+borrowed_by_number_id INT DEFAULT NULL
 )
 """)
+
+conn.commit()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS members(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50),
+    email VARCHAR(50) NOT NULL UNIQUE,
+    is_active BOOLEAN DEFAULT FALSE NOT NULL,
+    total_borrows INT NOT NULL)
+"""
+)
+
+conn.commit()
+
+cursor.close()
+conn.close()
