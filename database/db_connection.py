a71d76496ceb0680a19e9687a29f210bd88355b6
david stern <dzs101010@gmail.com>
2026-06-11 21:08:12 +0300

    create funcconnection  db_connection

--- database/db_connection.py
+++ database/db_connection.py
@@ -1,39 +1,41 @@
 import mysql.connector
 
-conn = mysql.connector.connect(
-    host="localhost",
-    port=3309,
-    user="root",
-    password="secret",
-    database="library_db"
-)
 
-cursor = conn.cursor()
+def get_connection():
+    conn = mysql.connector.connect(
+        host="localhost",
+        port=3309,
+        user="root",
+        password="secret",
+        database="library_db"
+    )
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS books (
-id INT AUTO_INCREMENT PRIMARY KEY,
-title VARCHAR(50) UNIQUE,
-author VARCHAR(50) UNIQUE,
-genre ENUM('Fiction', 'Non-Fiction', 'Science', 'History', 'Other'),
-is_available BOOLEAN DEFAULT TRUE,
-borrowed_by_number_id INT DEFAULT NULL
-)
-""")
+    cursor = conn.cursor()
 
-conn.commit()
-
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS members(
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS books (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50),
-    email VARCHAR(50) NOT NULL UNIQUE,
-    is_active BOOLEAN DEFAULT FALSE NOT NULL,
-    total_borrows INT NOT NULL)
-"""
-)
+    title VARCHAR(50) UNIQUE,
+    author VARCHAR(50) UNIQUE,
+    genre ENUM('Fiction', 'Non-Fiction', 'Science', 'History', 'Other'),
+    is_available BOOLEAN DEFAULT TRUE,
+    borrowed_by_number_id INT DEFAULT NULL
+    )
+    """)
 
-conn.commit()
+    conn.commit()
 
-cursor.close()
-conn.close()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS members(
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(50),
+        email VARCHAR(50) NOT NULL UNIQUE,
+        is_active BOOLEAN DEFAULT FALSE NOT NULL,
+        total_borrows INT NOT NULL)
+    """
+    )
+
+    conn.commit()
+
+    cursor.close()
+    conn.close()
